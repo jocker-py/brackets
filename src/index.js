bcb009105098cbe3413a6e11f61f1da8fbea6925
jocker-py <76263560+jocker-py@users.noreply.github.com>
2022-02-24 14:24:09 +0300

    feat: add solution of brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+    const stack = [];
+    const open_brackets = [];
+    const brackets_pair = {};
+    
+    for (let i in bracketsConfig) {
+        open_brackets.push(bracketsConfig[i][0]);
+        brackets_pair[bracketsConfig[i][1]] = bracketsConfig[i][0];
+    }
+    
+    for (let i in str) {
+        
+        if (open_brackets.includes(str[i])) {
+            if (brackets_pair[str[i]] === str[i] && brackets_pair[str[i]] === stack[stack.length - 1]) {
+                stack.pop()
+            } else {
+                stack.push(str[i]);
+            }
+
+        } else {
+            if (stack.length === 0) {
+                return false;
+            }
+
+            if (brackets_pair[str[i]] === stack[stack.length - 1]) {
+                stack.pop();
+            } else {
+                return false
+            }
+        }
+    }
+  
+    return stack.length === 0;
 }
+
+
+
